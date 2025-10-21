4fc906f230e97050ff2a81d786617c22d4b173f1
ludwig <ludwigstridh@gmail.com>
2025-10-21 12:59:32 +0200

    typo on import

--- src/pages/Receipt.jsx
+++ src/pages/Receipt.jsx
@@ -1,13 +1,13 @@
-import Header from '../components/Header'
-import ReceiptInfo from '../components/receiptInfo'
+import Header from "../components/Header";
+import ReceiptInfo from "../components/ReceiptInfo";
 
 const Receipt = () => {
-  return (
-    <div className='bg-[#605858] h-full flex flex-col items-center '>
-      <Header/>
-      <ReceiptInfo/>
-    </div>
-  )
-}
+	return (
+		<div className="bg-[#605858] h-full flex flex-col items-center ">
+			<Header />
+			<ReceiptInfo />
+		</div>
+	);
+};
 
-export default Receipt
+export default Receipt;
